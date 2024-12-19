e39a3790bcc1bf9c93a2725f938c770a07094ae0
lilly <lilly.koller@icloud.com>
2024-12-19 09:59:02 +0100

    changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const word1 = generate({ exactly: 1, minLength: 10, wordsPerString: 1 })[0];
 const word2 = generate();
 
 console.log(`The ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`);
+console.log("Hello this build should be green");
 
 export const sum = (a, b) => a + b;
 
